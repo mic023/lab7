ffa6fbd08b740b1313a0a87d1a6823b91278616b
vagrant <vagrant@precise32.(none)>
2015-02-19 20:26:03 +0000

    putting it together lab

--- models.js
+++ models.js
@@ -4,6 +4,10 @@ var Mongoose = require('mongoose');
 
 var ProjectSchema = new Mongoose.Schema({
   // fields are defined here
+  title: String,
+  date:{type:Date, default: Date.now},
+  summary: String,
+  image: Mongoose.Schema.Types.Mixed
 });
 
 exports.Project = Mongoose.model('Project', ProjectSchema);
--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,12 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  
   // query for the specific project and
   // call the following callback
+  models.Project
+      .find({"title": "Waiting in Line"})
+      .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -15,7 +18,17 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newProject = new models.Project({
+    "title": form_data.title,
+    "date": form_data.date,
+    "summary": form_data.summary
+  })
+  newProject.save(afterSaving);
 
+  function afterSaving(err){
+    if(err){console.log(err);ressend(500);}
+    res.redirect('/');
+  }
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -25,4 +38,13 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+      .find({"id": projectID})
+      .remove({"id":projectID})
+      .exec(afterRemoving);
+
+  function afterRemoving(err){
+    if(err) console.log(err);
+    res.json("OK");
+  }
 }
